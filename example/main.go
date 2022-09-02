6da625b2793d7347b1d1e8521b8ebb09c9867628
Benjamin Strelow <benjamin.strelow@fino.digital>
2022-09-02 16:53:17 +0200

    added writeHeader and checkrequestIDResty

--- example/main.go
+++ example/main.go
@@ -1,39 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"dev.azure.com/kycnow/kycnow/_git/tracygo"
-	"github.com/savsgio/atreugo/v11"
 
+	"dev.azure.com/kycnow/kycnow/_git/tracygo"
+	"github.com/go-resty/resty/v2"
+	"github.com/savsgio/atreugo/v11"
 )
 
 func main() {
 
-	router := atreugo.New(atreugo.Config{
-		Addr : "0.0.0.0:8080",
-	})
+	//router := atreugo.New(atreugo.Config{
+	//	Addr: "0.0.0.0:8080",
+	//})
 
-	router.UseBefore(tracygo.CheckRequestID)
-	router.UseAfter(tracygo.WriteHeaders)
-	router.GET("/hello-world", SomeFunction)
-	router.ListenAndServe()
-	
-	//client := resty.New()
-	
-	//_, err := client.R().
-	//	EnableTrace().
-	//	Get("http://localhost:8080")
-	//if err != nil {
-	//	fmt.Printf("error")
-	//}
+	// router.UseBefore(tracygo.CheckRequestID)
+	// router.UseAfter(tracygo.WriteHeader)
+	// router.GET("/hello-world", SomeFunction)
+	// router.ListenAndServe()
 
-	//fmt.Printf(string(resp))
+	client := resty.New()
+	client.OnBeforeRequest(tracygo.CheckRequestIDResty)
+	_, err := client.R().
+		SetContext(context.WithValue(context.Background(), "X-Correlation-ID", "Zitronenbaum")).
+		EnableTrace().
+		Get("http://localhost:8080/hello-world")
+	if err != nil {
+		fmt.Printf("error")
+	}
 
 }
 
 func SomeFunction(ctx *atreugo.RequestCtx) error {
 	fmt.Println("Hello world")
-	fmt.Println(ctx.UserValue("X-Request-ID").(string))
-	fmt.Println(ctx.UserValue("X-Correlation-ID").(string))
 	return nil
 }
--- tracy.go
+++ tracy.go
@@ -1,6 +1,9 @@
 package tracygo
 
 import (
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
 	"github.com/google/uuid"
 	"github.com/savsgio/atreugo/v11"
 )
@@ -21,10 +24,33 @@ func CheckRequestID(ctx *atreugo.RequestCtx) error {
 	return ctx.Next()
 }
 
-func WriteHeaders(ctx *atreugo.RequestCtx) error{
-	correlationId := ctx.UserValue("X-Correlation-ID")
-	requestId := ctx.UserValue("X-Request-ID")
+func WriteHeader(ctx *atreugo.RequestCtx) error {
+	correlationId := ctx.UserValue("X-Correlation-ID").(string)
+	requestId := ctx.UserValue("X-Request-ID").(string)
 
-	header := ctx.Response.Header.Add("X-Correlation-ID",correlationId)
+	ctx.Response.Header.Set("X-Correlation-ID", correlationId)
+	ctx.Response.Header.Set("X-Request-ID", requestId)
 
+	fmt.Println(ctx.Response.Header.String())
+
+	return ctx.Next()
+}
+
+func CheckRequestIDResty(client *resty.Client, request *resty.Request) error {
+	correlationId, ok := request.Context().Value("X-Correlation-ID").(string)
+	if !ok {
+		return nil
+	}
+
+	requestId, ok := request.Context().Value("X-Request-ID").(string)
+	if !ok {
+		return nil
+	}
+
+	if correlationId != "" || requestId != "" {
+		request.Header.Set("X-Correlation-ID", correlationId)
+		request.Header.Set("X-Request-ID", requestId)
+	}
+
+	return nil
 }
