e2503b04a96dac921a6b154f45ea5612adde0de0
Nico <nicholai.gerschewsky@clarilab.de>
2023-10-08 21:14:44 +0200

    chore: rewrite to v2

---
+++ options.go
@@ -0,0 +1,18 @@
+package tracygo
+
+// Option is an optional func.
+type Option func(tracy *TracyGo)
+
+// WithCorrelationID returns a function that sets the key for the correlationId header.
+func WithCorrelationID(id string) Option {
+	return func(tracy *TracyGo) {
+		tracy.correlationID = CorrelationID(id)
+	}
+}
+
+// WithRequestID returns a function that sets the key for the requestId header.
+func WithRequestID(id string) Option {
+	return func(tracy *TracyGo) {
+		tracy.requestID = RequestID(id)
+	}
+}
