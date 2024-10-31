501d9c89ef78225cf796fed624d7f0364d82d617
Felix Gehrmann <felix.gehrmann@kycnow.de>
2024-10-31 14:35:08 +0100

    rework eh middleware to use wrapping functions because compiler does not allow casting of named types with same structure

--- middleware/eventhorizon/middlewares.go
+++ middleware/eventhorizon/middlewares.go
@@ -6,25 +6,29 @@ import (
 	"github.com/Clarilab/tracygo/v2"
 )
 
-type (
-	contextMarshalFunc   func(context.Context, map[string]any)
-	contextUnmarshalFunc func(context.Context, map[string]any) context.Context
-)
-
 // CheckTracingIDs registers marshal and unmarshal functions for correlationIDs in contextes.
-// The parameters marshalFunc and unmarshalFunc are to be filled with the RegisterMarshal/UnmarshalFunc function of your eventhorizon library.
-func CheckTracingIDs(tracer *tracygo.TracyGo, registerMarshal func(f contextMarshalFunc), registerUnmarshal func(f contextUnmarshalFunc) context.Context) {
-	registerMarshal(func(ctx context.Context, vals map[string]any) {
+// The parameter functions registerMarshal and registerUnmarshal are supposed to be filled with functions,
+// that wrap around your respective eventHorizon libraries call to RegisterContextMarshaler/RegisterContextUnmarshaler
+func CheckTracingIDs(
+	tracer *tracygo.TracyGo,
+	registerMarshal func(func(context.Context, map[string]any)),
+	registerUnmarshal func(func(context.Context, map[string]any) context.Context),
+) {
+	marshalFunc := func(ctx context.Context, vals map[string]any) {
 		if correlationID, ok := ctx.Value(tracer.CorrelationIDKey()).(string); ok {
 			vals[tracer.CorrelationIDKey()] = correlationID
 		}
-	})
+	}
 
-	registerUnmarshal(func(ctx context.Context, vals map[string]any) context.Context {
+	registerMarshal(marshalFunc)
+
+	unmarshalFunc := func(ctx context.Context, vals map[string]any) context.Context {
 		if correlationID, ok := vals[tracer.CorrelationIDKey()].(string); ok {
 			ctx = context.WithValue(ctx, tracer.CorrelationIDKey(), correlationID)
 		}
 
 		return ctx
-	})
+	}
+
+	registerUnmarshal(unmarshalFunc)
 }
